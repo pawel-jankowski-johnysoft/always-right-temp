91a9bf2c63c87d83f4f6b2016634aa7018f45649
johnthebest <pawel.jankowski.johnysoft@gmail.com>
2024-03-19 14:34:55 +0100

    Ad support for avro schema, reuse "schemas" subproject, change add script to build docker image, update docker compose to use new service

--- temperature_anomaly_analyzer/src/main/java/com/johnysoft/temperature_anomaly_analyzer/detect/AnomalyDetectionConfiguration.java
+++ temperature_anomaly_analyzer/src/main/java/com/johnysoft/temperature_anomaly_analyzer/detect/AnomalyDetectionConfiguration.java
@@ -1,7 +1,11 @@
 package com.johnysoft.temperature_anomaly_analyzer.detect;
 
+import com.johnysoft.measurement_generator.TemperatureMeasurement;
 import com.johnysoft.temperature_anomaly_analyzer.kafka.SerdesFactories;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
@@ -12,6 +16,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Collections;
 
 @Configuration
 class AnomalyDetectionConfiguration {
@@ -27,18 +33,23 @@ class AnomalyDetectionConfiguration {
     }
 
     @Bean
-    public Topology detectAnomalyTopology(StreamsBuilder streamsBuilder,
-                                          @Value("${anomaly.last_recent_measurements}") int lastRecentMeasurements,
-                                          @Value("${anomaly.anomaly_threshold}")int anomalyThreshold) {
-        var stream = streamsBuilder.stream(TEMPERATURE_MEASUREMENTS, Consumed.with(Serdes.Long(), SerdesFactories.fromJSONSerdes(TemperatureMeasurement.class)));
-        stream.groupByKey()
-                .aggregate(() -> AnomalyDetector.forMeasurementsWithThreshold(lastRecentMeasurements, anomalyThreshold),
-                        (key, value, aggregate) -> aggregate.process(value),
-                        Materialized.with(Serdes.Long(), SerdesFactories.fromJSONSerdes(AnomalyDetector.class)))
+    Serde<TemperatureMeasurement> anomalyDetectedValueSerde(@Value("${spring.kafka.properties.schema.registry.url:http://localhost:8081}") String schemaRegistry) {
+        Serde<TemperatureMeasurement> serde = new SpecificAvroSerde<>();
+        serde.configure(Collections.singletonMap(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,schemaRegistry), false);
+        return serde;
+    }
+
+    @Bean
+    Topology detectAnomalyTopology(StreamsBuilder streamsBuilder, Serde<TemperatureMeasurement> anomalyDetectedValueSerde, @Value("${anomaly.last_recent_measurements}") int lastRecentMeasurements, @Value("${anomaly.anomaly_threshold}") int anomalyThreshold) {
+
+        var stream = streamsBuilder.stream(TEMPERATURE_MEASUREMENTS, Consumed.with(Serdes.Long(), anomalyDetectedValueSerde));
+        stream.mapValues(InternalTemperatureMeasurement::from)
+                .groupByKey()
+                .aggregate(() -> AnomalyDetector.forMeasurementsWithThreshold(lastRecentMeasurements, anomalyThreshold), (key, value, aggregate) -> aggregate.process(value), Materialized.with(Serdes.Long(), SerdesFactories.fromJSONSerdes(AnomalyDetector.class)))
                 .filter((key, value) -> value.anomalyDetected())
-                .mapValues(AnomalyDetector::getAnomaly)
+                .mapValues((unused, value) -> value.getAnomaly().toTemperatureMeasurement())
                 .toStream()
-                .to(ANOMALY_DETECTED, Produced.with(Serdes.Long(), SerdesFactories.fromJSONSerdes(TemperatureMeasurement.class)));
+                .to(ANOMALY_DETECTED, Produced.with(Serdes.Long(), anomalyDetectedValueSerde));
 
         return streamsBuilder.build();
     }
--- temperature_anomaly_analyzer/src/main/java/com/johnysoft/temperature_anomaly_analyzer/detect/TemperatureMeasurement.java
+++
@@ -1,9 +0,0 @@
-package com.johnysoft.temperature_anomaly_analyzer.detect;
-
-import lombok.With;
-
-import java.util.Date;
-
-@With
-record TemperatureMeasurement(long roomId, long thermometerId, float temperature, Date timestamp) {
-}
